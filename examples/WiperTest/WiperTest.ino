9a5026313cfcdbc6697cd36c7b02e4d57e9e5f7f
GitModu <batatas@gmail.com>
2021-02-09 16:39:24 +0000

    Clean-up example.

--- examples/WiperTest/WiperTest.ino
+++ examples/WiperTest/WiperTest.ino
@@ -1,8 +1,7 @@
-/*
-Example project, using a X9CXXX digital potentiometer as a voltage divider,
-and measuring the output with an analog pin.
-
-*/
+///
+///	Example project, using a X9CX104 digital potentiometer as a voltage divider
+///	and measuring the output with an analog pin.
+/// 
 
 #include <FastX9CXXX.h>
 
@@ -16,11 +15,14 @@ and measuring the output with an analog pin.
 
 FastX9C104 Potentiometer(X9_CS_PIN, X9_UD_PIN, X9_INC_PIN);
 
+uint16_t Angle = 0;
+
 void setup() {
 	Serial.begin(9600);
 	Serial.println();
 	//Serial.print(F("X9C104 Digital Potentiometer Sine Wave Test."));
 
+	// Setup output read, analog pin.
 	pinMode(WIPER_ANALOG_PIN, INPUT);
 
 	// Reset potentiometer to known position (Step == 0).
@@ -30,22 +32,23 @@ void setup() {
 	Serial.println(F("Step\tOutput"));
 }
 
-uint16_t Angle = 0;
 
 void loop() {
-	// Step in sine wave.
+	// Step from sine wave, ranged to available steps.
 	const uint8_t step = (0.5 * sin(Angle / 5.0) * X9CXXX::X9_STEPS) + (X9CXXX::X9_STEPS / 2);
 	Potentiometer.JumpToStep(step);
 
+	// Get the estimated step from the mapped output voltage.
 	const uint8_t result = map(analogRead(WIPER_ANALOG_PIN), 0, 1023, 0, X9CXXX::X9_STEPS);
 
+	// Plot the sample.
 	Serial.print(step);
 	Serial.print('\t');
 	Serial.print(result);
 	Serial.print('\t');
 	Serial.println();
 
-	// Skip some steps to slow down sine wave.
+
 	Angle++;
 
 	delay(STEP_DELAY_MILLIS);    // wait for a bit before stepping.
